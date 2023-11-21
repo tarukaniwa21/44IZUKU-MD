305aea9125693b18e0f888514d50687fb9c65cd4
tarukaniwa21 <146523178+tarukaniwa21@users.noreply.github.com>
2023-11-21 15:55:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ module.exports = {
   BRANCH: 'main', 
   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
   afk:  process.env.AFK ||true,
-  autobio:  process.env.AUTO_BIO === undefined ? '' : process.env.AUTO_BIO,
+  autobio:  process.env.AUTO_BIO === undefined ? 'true' : process.env.AUTO_BIO,
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
   HEROKU: {
